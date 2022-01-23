f4db4f655b79c43c784ae448b0f2d2c42204b3fa
Maximac1234 <maximac99+github@gmail.com>
2022-01-23 11:54:02 +0100

    -

--- psbd_projekt/src/Ekrany/EkranKlienta.java
+++ psbd_projekt/src/Ekrany/EkranKlienta.java
@@ -338,7 +338,8 @@ public class EkranKlienta extends javax.swing.JFrame {
     }//GEN-LAST:event_wylogujButtonActionPerformed
 
     private void historiaZamowienButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_historiaZamowienButtonActionPerformed
-        // TODO add your handling code here:
+        new Ekran_historii_zamowien().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_historiaZamowienButtonActionPerformed
 
     private void wlasnyProjektButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wlasnyProjektButtonActionPerformed
