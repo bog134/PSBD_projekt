f6f36acd861d76eec523f2566b0578fcfa097052
bog134 <81586760+bog134@users.noreply.github.com>
2022-01-30 05:09:45 +0100

    Update Wyzwalacz.sql

--- Baza_danych/Wyzwalacz.sql
+++ Baza_danych/Wyzwalacz.sql
@@ -4,10 +4,24 @@ SET GLOBAL event_scheduler="ON";
 
 CREATE EVENT `Generuj liste potrzebnych materialow` ON SCHEDULE EVERY 1 DAY ON COMPLETION NOT PRESERVE ENABLE DO 
 
-SELECT material_mebel.Id_Materialu, SUM(material_mebel.Ilosc)
-FROM material_mebel
-JOIN mebel USING (Id_Mebla)
-JOIN zamowienie_na_meble USING (Id_Zamowienia)
-WHERE zamowienie_na_meble.Czas_realizacji_Data_zlozenia=CURRENT_DATE
-GROUP BY Id_Materialu
+SELECT tab.Id_Materialu, SUM(tab.Ilosc)
+FROM
+(
+	SELECT material_mebel.Id_Materialu, SUM(material_mebel.Ilosc) AS Ilosc
+	FROM material_mebel
+	JOIN mebel USING (Id_Mebla)
+	JOIN zamowienie_na_meble USING (Id_Zamowienia)
+	WHERE Id_Proj_klient is null
+	GROUP BY Id_Materialu
+	UNION
+	SELECT material_mebel.Id_Materialu, SUM(material_proj_klienta.Ilosc)
+	FROM material_mebel
+	JOIN mebel USING (Id_Mebla)
+	JOIN zamowienie_na_meble USING (Id_Zamowienia)
+	JOIN material_proj_klienta USING (Id_Proj_klient)
+	WHERE Id_Proj_klient is not null
+	GROUP BY Id_Materialu
+) AS tab
+GROUP BY tab.Id_Materialu
+ORDER BY tab.Id_Materialu 
 
